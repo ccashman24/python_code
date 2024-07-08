87988527710dc4ac5e5fb851054ae77fa5f879ee
ccashman24 <ccashman24@gmail.com>
2024-07-07 23:29:50 -0400

    commit on "main"

--- capital_one_stock.py
+++ capital_one_stock.py
@@ -1,18 +1,24 @@
+'''
+CAPITAL ONE STOCK PRICE
+LAST UPDATED: 2024-06-30
+'''
+
 import yfinance as yf
 import xlsxwriter as excel
 import pandas
 from datetime import datetime
-
+import matplotlib.pyplot as plt
 print()
 
-#import matplotlib.pyplot as plt
+# DEFINE THE STOCK SYMBOL - CAPITAL ONE (COF)
+stock_symbol = 'COF'
 
-# Define the stock symbol (Capital One)
-stock_symbol = "COF"
+def get_current_price(stock_symbol):
+    cof_ticker = yf.Ticker(stock_symbol)
+    print(get_current_price)
 
-#def get_current_price(stock_symbol):
-#    cof_ticker = yf.Ticker(stock_symbol)
-
+# get_current_price(stock_symbol)
+# print(get_current_price)
 
 cof_ticker = yf.Ticker(stock_symbol).info
 
@@ -22,8 +28,8 @@ market_price = cof_ticker['currentPrice']
 previous_close_price = cof_ticker['regularMarketPreviousClose']
 
 print('Stock: Capital One (COF)')
-print('Current Market Price:', market_price)
-print('Previous Close Price:', previous_close_price)
+print('Current Market Price: $', market_price)
+print('Previous Close Price: $', previous_close_price)
 
 
 
